1202fa71c11a2150e8c456a94aaaa137518aecd9
王志坤 <cheekhan@foxmail.com>
2022-09-13 17:02:55 +0800

    design模块：更改构建配置

--- packages/wzk-design/src/index.ts
+++ packages/wzk-design/src/index.ts
@@ -1,10 +1,21 @@
+import {createApp} from "vue"
 import 'element-plus/dist/index.css';
 
-import wzkLogin from './components/wzkLogin/index.vue';
 import wzkImgCut from "./components/wzkImgCut/index.vue";
+// import wzkCropper from "./components/wzkCropper/index.vue";
+import wzkLogin from "./components/wzkLogin/index.vue";
 
-export {wzkLogin, wzkImgCut};
 
-export default {
-    wzkLogin, wzkImgCut
-};
+export {
+    wzkImgCut,
+    // wzkCropper,
+    wzkLogin,
+}
+
+export default function xkDesignElement() {
+    return {
+        wzkImgCut: createApp(wzkImgCut),
+        // wzkCropper: createApp(wzkCropper),
+        wzkLogin: createApp(wzkLogin)
+    }
+}
--- packages/wzk-design/vite.config.ts
+++ packages/wzk-design/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from "path"
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  mode: "production",
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      name: 'XkDesign',
+      // the proper extensions will be added
+      fileName: 'xk-design'
+    }
+  },
+  define: {
+    'process.env.NODE_ENV': "'production'"
+  }
 })
